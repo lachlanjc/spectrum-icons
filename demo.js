2ac914262ba60703a62fd51bc69125627d8373ec
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-04-29 00:13:59 -0400

    Add demo page

---
+++ demo.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Icon, { glyphNames } from './index.js'
+
+export default () => (
+  <div
+    style={{
+      display: 'grid',
+      gridTemplateColumns: 'repeat(auto-fill, minmax(48px, 1fr))',
+      gridGap: 16,
+      padding: 32,
+      maxWidth: 768,
+      margin: 'auto'
+    }}
+  >
+    <title>spectrum-icons</title>
+    {glyphNames.map(key => (
+      <Icon glyph={key} title={key} size={48} key={key} />
+    ))}
+  </div>
+)
